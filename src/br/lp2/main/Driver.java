38827379834c7150c97b9ff364444a538c499a46
Pedro H. B. Cavalcante <pedrinho.sjs@ufrn.edu.br>
2016-11-10 09:32:55 -0300

    Solved problems

--- src/br/lp2/main/Driver.java
+++ src/br/lp2/main/Driver.java
@@ -1,11 +1,24 @@
 package br.lp2.main;
+import java.awt.Component;
+import java.io.File;
+
+import javax.swing.JFileChooser;
+
+import br.lp2.player.PlayerFile;
 import br.lp2.view.*;
 @SuppressWarnings("unused")
 public class Driver {
-
+	final static JFileChooser chooser = new JFileChooser();
+	static Component parent = null;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		GUI ops = new GUI();
+		try{
+			chooser.showOpenDialog(parent);
+		}catch (NullPointerException e ){
+			System.out.println("erro1");
+		}
+		File caminho = chooser.getSelectedFile();
+		PlayerFile music = new PlayerFile(caminho);
 	}
 
 }
--- src/br/lp2/player/PlayerFile.java
+++ src/br/lp2/player/PlayerFile.java
@@ -1,27 +1,33 @@
 package br.lp2.player;
-import javazoom.jl.player.*;
+import javazoom.jl.player.Player;
+
+import java.awt.Component;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.ArrayList;
+
+import javax.swing.JFileChooser;
+
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 
 @SuppressWarnings("unused")
-public class Player {
-	 private File music;
-	 private Player player;
+public class PlayerFile {
+	File music_fl;
+	Player player;
 	
-	public Player(File musicFile){
-		this.music = musicFile;
+	public PlayerFile(File musicFile){
+		this.music_fl = musicFile;
 	}
 	public void Play(){
 		try{
-			FileInputStream stream = new FileInputStream(music);
+			FileInputStream stream = new FileInputStream(music_fl);
             BufferedInputStream buffer = new BufferedInputStream(stream);
             this.player = new Player(buffer);
             System.out.println("Executando...");
-            this.player.Play();
+            this.player.play();
             System.out.println("Terminado");
 		}catch (Exception e){
 			System.out.println("ops");
